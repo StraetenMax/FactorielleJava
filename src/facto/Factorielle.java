125eec0ec02fcf99da234bbe9399a7842c1ec85a
StraetenMax <maxstraeten@yahoo.com>
2017-03-28 14:03:59 +0200

    finalisation du projet

--- src/facto/Factorielle.java
+++ src/facto/Factorielle.java
@@ -1,25 +1,24 @@
 package facto;
 
 import java.util.Scanner;
+import java.lang.*;
 
 public class Factorielle {
 
 	public static void main(String[] arg) {		
 		int n;
 		int factorielle =1;
+		String nbre = "inconnu";
 		
-		System.out.println("Entrez un nombre");
+		System.out.println("Entrez un nombre, s'il vous plaît :");
 		Scanner clavier = new Scanner(System.in);
-		n = clavier.nextInt();
+		nbre = clavier.nextLine();
 		
-		
-		
-		
-		//n = Integer.parseInt(arg[0]);
-		//for(int i=2; i<n; i++ ){  
-		//	factorielle *= i;
-		//System.out.println("La factorielle du nombre "+n+" est de "+factorielle);
-		//}
+		n = Integer.parseInt(nbre);
+		for(int i=2; i<n; i++ ){  
+		factorielle *= i;
+		System.out.println("La factorielle de "+n+" est "+factorielle);
+		 }
 		}
 
 }
