37b6ca4fdffef3fcd285fefddaf869c7976fbf8b
StraetenMax <maxstraeten@yahoo.com>
2017-03-28 13:34:46 +0200

    préparation du fichier

---
+++ src/facto/Factorielle.java
@@ -0,0 +1,25 @@
+package facto;
+
+import java.util.Scanner;
+
+public class Factorielle {
+
+	public static void main(String[] arg) {		
+		int n;
+		int factorielle =1;
+		
+		System.out.println("Entrez un nombre");
+		Scanner clavier = new Scanner(System.in);
+		n = clavier.nextInt();
+		
+		
+		
+		
+		//n = Integer.parseInt(arg[0]);
+		//for(int i=2; i<n; i++ ){  
+		//	factorielle *= i;
+		//System.out.println("La factorielle du nombre "+n+" est de "+factorielle);
+		//}
+		}
+
+}
